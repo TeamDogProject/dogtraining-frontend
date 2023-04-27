980a80dcf78e561507815b832e1041713bfcc5a0
AdayOrtega <aday.ortega@gmail.com>
2023-04-27 17:10:54 +0100

    singUp process

--- src/Page/Home/Signup/Signup.jsx
+++ src/Page/Home/Signup/Signup.jsx
@@ -5,25 +5,56 @@ import React, { useState } from 'react'
 function Signup() {
 
     const [name, setName] = useState('')
+    const [surname, setSurName] = useState('')
+     const [userName, setUserName] = useState('')
 
      const handleChangeName = (e)=>{
-        setName(e.targe.value)
+        setName(e.target.value)
+    }
+
+    const handleChangeSurname = (e)=>{
+        setSurName(e.target.value)
     }
      
+    const handleChangeUserName = (e)=>{
+        setUserName(e.target.value)
+    }
 
 
   return (
     <>
       <div className="containerSignUp">
-        <Card sx={{height:'80vh' , width: '100vw', backgroundColor: 'white' }} raised={true}>
+        <Card
+          sx={{
+            height: '80vh',
+            width: '100vw',
+            backgroundColor: 'white',
+            padding: 1,
+          }}
+          raised={true}
+        >
           <CardHeader title="Registration Form"></CardHeader>
           <TextField
             label="Name"
             variant="outlined"
-            margin='dense'
+            margin="dense"
             onChange={handleChangeName}
-            
-          ></TextField>
+          />
+          <TextField
+            sx={{ marginLeft: 2 }}
+            label="Surname"
+            variant="outlined"
+            margin="dense"
+            onChange={handleChangeSurname}
+          />
+
+          <TextField
+            sx={{ marginLeft: 2 }}
+            label="UserName"
+            variant="outlined"
+            margin="dense"
+            onChange={handleChangeUserName}
+          />
         </Card>
       </div>
     </>
