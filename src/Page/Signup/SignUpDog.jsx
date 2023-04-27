e8922fe2636a799d99b94c3254cb3e3f35fadf1f
Aday Ortega Rodríguez <adayortega@MBP-de-Aday.home>
2023-04-28 00:14:20 +0100

    Form Page with user Form and Dog Form are Done

--- src/Page/Signup/SignUpDog.jsx
+++ src/Page/Signup/SignUpDog.jsx
@@ -1,41 +1,51 @@
-import { Card, CardHeader, TextField, IconButton, Divider, CardActions, CardContent, Button } from '@mui/material'
-import { blue } from '@mui/material/colors'
+import {
+  Card,
+  CardHeader,
+  TextField,
+  IconButton,
+  Divider,
+  CardActions,
+  CardContent,
+  Button,
+  Box,
+  Container,
+  Typography,
+} from '@mui/material'
 import React, { useState } from 'react'
-import { VisibilityOff, Lock, Visibility } from '@mui/icons-material'
+import PetsIcon from '@mui/icons-material/Pets'
 
 function SignUpDog() {
+  const [Dogname, seDogtName] = useState('')
+  const [dogAge, setDogAge] = useState('')
+  const [dogBreed, setDogBreed] = useState('')
+  const [dogChip, setDogChip] = useState('')
+  const [dogProblem, setDopProblem] = useState('')
 
-    const [Dogname, seDogtName] = useState('')
-    const [dogAge, setDogAge] = useState('')
-    const [dogBreed, setDogBreed] = useState('')
-    const [dogChip, setDogChip] = useState('')
-    const [dogProblem, setDopProblem] = useState('')
+  const [isPassVisible, setIsPassVisible] = useState(false)
 
-    const [isPassVisible, setIsPassVisible] = useState(false)
-
-  const handlePass = (e)=>{
-        setIsPassVisible(!isPassVisible)
+  const handlePass = (e) => {
+    setIsPassVisible(!isPassVisible)
   }
- 
-     const handleChangeDogName = (e)=>{
-        setDogName(e.target.value)
-    }
 
-    const handleChangeDogBreed = (e)=>{
-        setDogBreed(e.target.value)
-    }
-     
-    const handleChangeDogAge = (e)=>{
-        setDogAge(e.target.value)
-    }
-    const handleChangeDogChip = (e)=>{
-        setDogChip(e.target.value)
-    }
+  const handleChangeDogName = (e) => {
+    setDogName(e.target.value)
+  }
 
-    const handleChangeDogProblem = (e) =>{
-        setDopProblem(e.target.value)
-    }
-    
+  const handleChangeDogBreed = (e) => {
+    setDogBreed(e.target.value)
+  }
+
+  const handleChangeDogAge = (e) => {
+    setDogAge(e.target.value)
+  }
+  const handleChangeDogChip = (e) => {
+    setDogChip(e.target.value)
+  }
+
+  const handleChangeDogProblem = (e) => {
+    setDopProblem(e.target.value)
+  }
+
   return (
     <>
       <div className="containerSignUp">
@@ -45,72 +55,84 @@ function SignUpDog() {
             width: '100vw',
             backgroundColor: 'white',
             padding: 1,
-            border:0
+            border: 0,
           }}
-         
         >
+          <CardContent>
+            <CardHeader title="Dog Registration Form"></CardHeader>
+            <TextField
+              sx={{ width: 300 }}
+              label="Dog Name"
+              variant="outlined"
+              margin="dense"
+              onChange={handleChangeDogName}
+            />
 
-        <CardContent>
-          <CardHeader title="Dog Registration Form"></CardHeader>
-          <TextField 
-            sx={{width: 300 }}
-            label="Dog Name"
-            variant="outlined"
-            margin="dense"
-            onChange={handleChangeDogName}
-          />
-          <TextField
-            sx={{ marginLeft: 2, width: 400  }}
-            label="Breed of dog"
-            variant="outlined"
-            margin="dense"
-            onChange={handleChangeDogBreed}
-          />
+            <TextField
+              sx={{ marginLeft: 2, width: 400 }}
+              label="Breed of dog"
+              variant="outlined"
+              margin="dense"
+              onChange={handleChangeDogBreed}
+            />
 
-          <TextField
-            sx={{ marginLeft: 2 , width: 250}}
-            label="Age"
-            variant="outlined"
-            margin="dense"
-            onChange={handleChangeDogAge}
-          />
+            <TextField
+              sx={{ marginLeft: 2, width: 250 }}
+              label="Age"
+              variant="outlined"
+              margin="dense"
+              onChange={handleChangeDogAge}
+            />
 
-          <TextField
-            sx={{ marginLeft: 2 }}
-            label="Chip Number"
-            variant="outlined"
-            margin="dense"
-            onChange={handleChangeDogChip}
-          />
+            <TextField
+              sx={{ marginLeft: 2 }}
+              label="Chip Number"
+              variant="outlined"
+              margin="dense"
+              onChange={handleChangeDogChip}
+            />
 
-          <TextField
-          sx={{  width: 600,  }}
-          label="Describe your dog's behavior problems"
-          variant="outlined"
-          margin="dense"
-          onChange={handleChangeDogProblem}
-        />
-           
-           </CardContent>
+            <TextField
+              sx={{ width: '84vw' }}
+              label="Describe your dog's behavior problems"
+              variant="outlined"
+              margin="dense"
+              onChange={handleChangeDogProblem}
+            />
 
-           <Divider sx={{paddingTop:4 }} />
+            <Card color='primary' variant="outlined" sx={{marginRight:1, padding: 1, fontFamily: 'roboto', display:'flex', justifyContent: 'flex-start', width:400 }}>
+            <Typography  color="text.secondary" sx={{paddingBottom:1,right:20, paddingTop:1 , display:'flex', flexDirection: 'row'}}>
+            <label htmlFor="upload-photo">
+            
+            Select a photo of your dog
+            
+            <input id="upload-photo" type="file" accept="image/*" />
 
-           <CardActions sx={{ display: ' flex', justifyContent: 'flex-center' }}>
+            </label>
+            </Typography>
+            </Card>
+            
+            
 
-           <Button
+
+          </CardContent>
+
+          <Divider sx={{ paddingTop: 4 }} />
+
+          <CardActions sx={{ display: ' flex', justifyContent: 'flex-center' }}>
+            <Button
               size="small"
               color="secondary"
               variant="contained"
-              sx={{ backgroundColor: '#088395', borderTop:2, marginLeft: 5 }}
+              sx={{ backgroundColor: '#088395', borderTop: 2, marginLeft: 5 }}
             >
-              Register User
+              Register Dog
             </Button>
-           </CardActions>
+          </CardActions>
         </Card>
       </div>
     </>
   )
 }
-
 
 export default SignUpDog
