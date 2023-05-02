8595d578e3a57cbea586ff71e4801df53323238f
Aday Ortega Rodríguez <adayortega@MBP-de-Aday.home>
2023-05-02 12:03:12 +0100

    last Change of Cesar

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import router from './router'
 import './assets/fonts/roboto'
 import { UserContext } from './context/user'
 import { useState } from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
 
-// Creating a client
 
-const queryClient = new QueryClient()
 
 function App() {
 
@@ -18,11 +15,11 @@ function App() {
   return (
     // Providing to hole the app the client
     <div className="App" style={{ margin:0, padding:0 }}>
-      <QueryClientProvider client={queryClient}>
+ 
         <UserContext.Provider value={obj}>
           <RouterProvider router={router} />
         </UserContext.Provider>
-      </QueryClientProvider>
+ 
     </div>
   )
 }
