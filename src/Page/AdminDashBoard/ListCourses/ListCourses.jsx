04e7ec8ac19b4864286e0ef41b911da7bf2ed777
djcesargarcia <djcesargarcia@gmail.com>
2023-05-08 09:55:11 +0100

    Add changes to node modules

--- src/Page/AdminDashBoard/ListCourses/ListCourses.jsx
+++ src/Page/AdminDashBoard/ListCourses/ListCourses.jsx
@@ -64,7 +64,7 @@ const ListCourses = () => {
   function displayCourses() {
     return (
       <>
-      <EditCourseForm close={handleClose} show={showModal} courseId={courseId} courseName={courseName} courseDuration={courseDuration} coursePrice={coursePrice} coursePlace={coursePlace} />
+      <EditCourseForm close={handleClose} show={showModal} courseId={courseId} courseName={courseName} courseDescription={courseDescription} courseDuration={courseDuration} coursePrice={coursePrice} coursePlace={coursePlace} />
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <Box>
