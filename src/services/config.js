d4b6084bb21d3c09d8ef695169a65a30ba99f5b6
djcesargarcia <djcesargarcia@gmail.com>
2023-05-12 11:59:32 +0100

    Add changes in baseURL

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   //baseURL: 'https://localhost:3000/api'
-  baseURL: 'https://dogtrainingapi.onrender.com/api',
+  baseURL: 'https://dogtraining-production.up.railway.app/api'
 })
 
 export default api
